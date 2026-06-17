6514182d9c00075b38f37c7705d4aac71515eb14
mngt <80961204+mngste@users.noreply.github.com>
2026-06-17 09:46:40 +0200

    Create configuration.nix

---
+++ hosts/thinkpad/configuration.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  networking.hostName = "thinkpad";
+
+  time.timeZone = "Europe/Paris";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # principal user
+  users.users.mngt = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
+    shell = pkgs.zsh;
+  };
+
+  # nix flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # network / audio / etc.
+  networking.networkmanager.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  services.printing.enable = true;
+
+  # gui wayland with niri
+  services.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
+
+  programs.niri.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    curl
+    fastfetch
+    btop
+    neovim
+    yazi
+    starship
+
+    alacritty
+    ghostty
+    fuzzel
+    mako
+    waybar
+  ];
+
+  programs.zsh.enable = true;
+
+  system.stateVersion = "26.05";
+}
