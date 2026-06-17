bef89b768215c0d1c23f90516186965aa59577e7
mngt <80961204+mngste@users.noreply.github.com>
2026-06-17 10:11:44 +0200

    Create packages.nix

---
+++ home/thinkpad/packages.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Shell / navigation
+    zoxide
+    starship
+    eza
+    bat
+    fd
+    ripgrep
+    fzf
+    lazygit
+    yazi
+
+    # Term / WM / outils
+    alacritty
+    ghostty
+    fastfetch
+    fuzzel
+    mako
+    waybar
+    neovim
+
+    # Dev / ops
+    docker
+  ];
+}
