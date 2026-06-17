b02870350942666b1a0877eeef2908ba3eabb4c1
mngt <80961204+mngste@users.noreply.github.com>
2026-06-17 13:00:23 +0200

    Update fonts.nix

--- home/thinkpad/fonts.nix
+++ home/thinkpad/fonts.nix
@@ -6,4 +6,12 @@
   home.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 22;
+  };
 }
