af1f030d639828d82fef9600c46500cdca1b4e8a
mngt <80961204+mngste@users.noreply.github.com>
2026-06-23 20:27:39 +0200

    Enable Mako and configure Waybar and Fuzzel

--- home/dell/home.nix
+++ home/dell/home.nix
@@ -6,6 +6,15 @@
     # ./dotfiles.nix   # xdg.configFile for niri
   ];
 
+  services.mako.enable = true;
+
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+  };
+
+  programs.fuzzel.enable = true;
+
   home.username = "mngt";
   home.homeDirectory = "/home/mngt";
   home.stateVersion = "26.05";
