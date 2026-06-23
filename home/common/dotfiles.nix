e89d67e264f3865e30094c63f61244ef8243b790
mngt <80961204+mngste@users.noreply.github.com>
2026-06-23 12:04:41 +0200

    Update dotfiles.nix

--- home/common/dotfiles.nix
+++ home/common/dotfiles.nix
@@ -3,6 +3,7 @@
 {
   xdg.configFile."fastfetch".source = ./files/fastfetch;
   xdg.configFile."fuzzel".source = ./files/fuzzel;
+  xdg.configFile."ghostty".source = ./files/ghostty;
   xdg.configFile."mako".source = ./files/mako;
   xdg.configFile."nvim".source = ./files/nvim;
   # xdg.configFile."waybar".source = ./files/waybar;
