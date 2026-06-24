4e8756b9749c9042d84c31400bd6cd50ee3c6df1
mngt <80961204+mngste@users.noreply.github.com>
2026-06-24 09:40:26 +0200

    Update dotfiles.nix

--- home/common/dotfiles.nix
+++ home/common/dotfiles.nix
@@ -5,8 +5,9 @@
   xdg.configFile."fuzzel".source = ./files/fuzzel;
   xdg.configFile."ghostty".source = ./files/ghostty;
   xdg.configFile."mako".source = ./files/mako;
+  xdg.configFile."noctalia".source = ./files/noctalia;
   xdg.configFile."nvim".source = ./files/nvim;
+  # xdg.configFile."rofi".source = ./files/rofi;
   # xdg.configFile."waybar".source = ./files/waybar;
   xdg.configFile."yazi".source = ./files/yazi;
-  # xdg.configFile."rofi".source = ./files/rofi;
 }
