25cadd71c11658705c0689ee057423b2c36cee00
mngt <80961204+mngste@users.noreply.github.com>
2026-06-24 20:11:14 +0200

    Update dotfiles.nix

--- home/thinkpad/dotfiles.nix
+++ home/thinkpad/dotfiles.nix
@@ -9,13 +9,11 @@
   # Niri
   xdg.configFile."niri/animation.kdl".source = ./files/niri/animation.kdl;
   xdg.configFile."niri/autostart.kdl".source = ./files/niri/autostart.kdl;
-  xdg.configFile."niri/backup.kdl".source = ./files/niri/backup.kdl;
   xdg.configFile."niri/bind.kdl".source = ./files/niri/bind.kdl;
   xdg.configFile."niri/config.kdl".source = ./files/niri/config.kdl;
   xdg.configFile."niri/input.kdl".source = ./files/niri/input.kdl;
   xdg.configFile."niri/layout.kdl".source = ./files/niri/layout.kdl;
   xdg.configFile."niri/misc.kdl".source = ./files/niri/misc.kdl;
-  xdg.configFile."niri/noctalia.kdl".source = ./files/niri/noctalia.kdl;
   xdg.configFile."niri/output.kdl".source = ./files/niri/output.kdl;
   xdg.configFile."niri/window-rule.kdl".source = ./files/niri/window-rule.kdl;
 
