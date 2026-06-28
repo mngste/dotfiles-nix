d5c260116d27b5c0a7f345927da2e1b9c7e967ea
mngt <80961204+mngste@users.noreply.github.com>
2026-06-28 22:33:56 +0200

    Add Proton Drive CLI to package list

--- home/common/packages.nix
+++ home/common/packages.nix
@@ -29,4 +29,20 @@
     firefox
     proton-pass-cli
   ];
+
+  let
+  protonDrive = pkgs.fetchurl {
+    url = "https://proton.me/download/drive/cli/0.4.6/linux-x64/proton-drive";
+    sha256 = "d187409932742e6fdc6aae2995998f4c89ea51999283395bc8d0bdc5343a79d31bf5a485d5af9adf3b7909fc92f2d2ef0b133edc4939d5faf1d096eb744425bb";
+  };
+  in {
+    home.packages = [
+      pkgs.libsecret
+      (pkgs.writeShellScriptBin "proton-drive" ''
+        exec ${protonDrive} "$@"
+      '')
+    ];
+  };
 }
+
+
