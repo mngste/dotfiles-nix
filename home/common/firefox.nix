238d0d6371ff725a39a09fc28bc092a9b027c0a6
mngste <mangouste@mosaurus.com>
2026-07-08 18:18:06 +0200

    edit config

--- home/common/firefox.nix
+++ home/common/firefox.nix
@@ -24,6 +24,7 @@
       extensions.packages =
         with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
           proton-pass
+          proton-vpn
           ghostery
         ];
     };
